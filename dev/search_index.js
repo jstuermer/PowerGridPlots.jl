b948b6c91f5d224f22413d35f4b69c58159adebb
Documenter.jl <documenter@juliadocs.github.io>
2022-12-21 12:19:34 +0000

    build based on ee57915

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PowerGridPlots","category":"page"},{"location":"#PowerGridPlots","page":"Home","title":"PowerGridPlots","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PowerGridPlots.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PowerGridPlots]","category":"page"}]
+}
